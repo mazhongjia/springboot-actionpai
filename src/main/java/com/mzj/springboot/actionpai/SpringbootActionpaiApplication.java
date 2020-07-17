41fb17b157409aa1d8affb02a37b3eebd9123fd5
mazhongjia <mazhongjia126@126.com>
2020-07-17 15:58:30 +0800

    20200717

--- src/main/java/com/mzj/springboot/actionpai/SpringbootActionpaiApplication.java
+++ src/main/java/com/mzj/springboot/actionpai/SpringbootActionpaiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-gpublic class SpringbootActionpaiApplication {
+public class SpringbootActionpaiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootActionpaiApplication.class, args);
